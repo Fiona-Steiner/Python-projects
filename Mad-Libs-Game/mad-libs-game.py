81009b8ac20c835a55d7cd3b4e8fa57d50f843ba
Steiner <fiona.steiner@gmail.com>
2022-11-04 11:39:43 +0100

    Ajout du projet

---
+++ Mad-Libs-Game/mad-libs-game.py
@@ -0,0 +1,9 @@
+adjectif = input("Veuillez entrer un 'adjectif' : ")
+prénom = input("Veuillez entrer un 'prénom' : ")
+adjectif2 = input("Veuillez entrer un second 'adjectif' : ")
+nom = input("Veuillez entrer un second 'nom' : ")
+nom2 = input("Veuillez entrer un second 'nom' : ")
+
+histoire = "\n\nPan pan pan,\nQui est là ?\nC'est la/le " +  adjectif + " " +  prénom + ".\n\nPan pan pan,\nQui est là ?\nC'est la/le " +  adjectif2 + " " +  prénom + ".\n\nJ'aurais besoin d'un " +  nom + " pour faire mon gâteau.\nJ'en n'ai pas, j'en n'ai pas.\nDébrouille-toi comme ça !\n\nJ'aurais besoin d'un " +  nom2 + " pour faire mon gâteau.\nJ'en n'ai pas, j'en n'ai pas.\nDébrouille-toi comme ça !"
+
+print(histoire)
